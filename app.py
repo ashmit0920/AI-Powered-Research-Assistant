3c8e6d1471161c73b32932b193b9ebaf3ee398f5
unknown <ashmitthawait2@gmail.com>
2024-06-28 02:54:02 +0530

    2 free searches, improved UI

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def display_main_app():
     st.write(f"Welcome! {st.session_state.username}")
     st.write("This website is designed to help you discover and summarize research papers on your topic of interest.")
     st.write(f"Enter the topic you are interested in and your Semantic Scholar API Key in the input fields on the sidebar. In case you don't have an API key, you can get it from [this link](https://www.semanticscholar.org/product/api).")
-    st.write(f"Wanna give it a try before getting your API Key? You can use the default API Key provided in the sidebar for 2 free searches!")
+    st.write(f"Wanna give it a try before getting your API Key? You can use our default API Key for 2 free searches!")
 
     # Sidebar
     st.sidebar.header("Research Settings")
@@ -100,17 +100,6 @@ def display_main_app():
                 api_key = DEFAULT_API
 
         search()
-
-    # if st.sidebar.button("Search"):
-    #     search()
-  
-    # if st.sidebar.button("Use a free search"):
-    #     if st.session_state.free_search >= 2:
-    #         st.sidebar.error("You have used all your free searches! Please enter your API Key to continue.")
-    #     else:
-    #         api_key = DEFAULT_API
-    #         st.session_state.free_search += 1
-    #         search()
 
 if __name__ == '__main__':
     main()
